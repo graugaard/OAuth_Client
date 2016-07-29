b246b696a6040904a0d97365c1b4ec9bdb7bb1ac
graugaard <portaldude@gmail.com>
2016-07-29 13:26:11 +0200

    Have now implemented that client requests resources and is granting a token.

--- src/ClientBean.java
+++ src/ClientBean.java
@@ -1,3 +1,6 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.json.JSONObject;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
@@ -5,6 +8,7 @@ import javax.faces.context.FacesContext;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,16 +17,17 @@ import java.net.URL;
  */
 @ManagedBean(name="client")
 @SessionScoped
-public class ClientBean {
+public class ClientBean implements Serializable{
     private String clientId = "7";
 
     private String secret = "1234567890";
 
     private String code;
-    private String domain = "http://graugaard.bobach.eu:8080/";
-    private String oauthLogin = domain + "OAuth/login.html";
-    private String oauthToken = domain + "OAuth/rest/oauth";
-    private String redirect = domain + "Client/client.jsf";
+    //private String domain = "http://graugaard.bobach.eu:8080/";
+    private String domain = "http://localhost:8080";
+    private String oauthLogin = domain + "/OAuth/login.html";
+    private String oauthToken = domain + "/OAuth/rest/oauth";
+    private String redirect = domain + "/Client/client.jsf";
     private String token = "";
     public String getCode() {
         return code;
@@ -40,31 +45,57 @@ public class ClientBean {
         return token;
     }
 
+    public boolean isValidCode() {
+        return !(code == null || code.equals(""));
+    }
+
+    public String getRedirectUrl() {
+        return oauthLogin + "?client_id=" + clientId
+                + "&redirect_uri=" + redirect
+                + "&permissions=fear,game";
+    }
+
+    public void getAuthCode() throws IOException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        context.redirect(oauthLogin + "?client_id=" + clientId
+                + "&redirect_uri=" + redirect
+                + "&permissions=fear,game");
+    }
+
     public void handleCode() throws IOException {
-        if (token.equals("") && (code == null || code.equals(""))) {
-            ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-            context.redirect(oauthLogin + "?client_id=" + clientId
-                    + "&redirect_uri=" + redirect
-                    + "&permissions=fear,game");
-        } else {
-            URL url = new URL(oauthToken +
+        if (token.equals("") && isValidCode()) {
+            String s = makeRequest(oauthToken +
                     "?code=" + code +
                     "&client_id=" + clientId +
                     "&client_secret=" + secret);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.connect();
+            JSONObject o = new JSONObject(s);
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-            StringBuilder builder = new StringBuilder();
-
-            String line = null;
-            while((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-
-            token = builder.toString();
+            token = o.getString("access_token");
         }
+    }
+
+    public String getInfoAboutUser() throws IOException {
+        if (!(token == null || token.equals(""))) {
+            return makeRequest(domain + "/OAuth/rest/resource?token=" + token);
+        }
+        return "";
+    }
+
+    private String makeRequest(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.connect();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        StringBuilder builder = new StringBuilder();
+
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+
+        return builder.toString();
     }
 }
